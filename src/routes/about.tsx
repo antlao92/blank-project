046992e1de5cec66160d322b4625a547c7f04c58
Anthony <anthonykimlao@gmail.com>
2024-09-25 06:28:43 -0500

    trying to get other colors and tailwind to work

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,5 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/about')({
-  component: () => <div>Hello Anthony! This is to show that the routing works</div>,
+  component: () => 
+  <div className='flex flex-col gap-2 text-blue-500'>
+    <div>Hello Anthony!</div>
+    <div>This is to show that the routing work</div>
+  </div>
 })
